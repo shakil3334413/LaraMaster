1e8246f67de0ea98dc5ade63bb3568fde932888c
shakil3334413 <shakil3334413@gmail.com>
2023-05-16 01:03:17 -0700

    Listing and Home page style Twailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
-    './resources/js/**/*.vue',
+    // './storage/framework/views/*.php',
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
   ],
   theme: {
     extend: {},
